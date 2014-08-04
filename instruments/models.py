cfdfc960125f563c1c5c5e7c9d2179454be24efb
Akshar Raaj <akshar@agiliq.com>
2014-08-04 20:25:34 +0530

    Instruments model created and registered with admin.
    * Instruments model created
    * Registered with admin.
    * Created a fixture with some Instruments.

--- instruments/models.py
+++ instruments/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Instrument(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
