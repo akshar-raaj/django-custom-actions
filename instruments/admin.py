a07cf356d8b04f23ed01f8ed83574418bfc30448
Akshar Raaj <akshar@agiliq.com>
2014-08-05 20:42:45 +0530

    Able to update price of selected rows from admin

--- instruments/admin.py
+++ instruments/admin.py
@@ -1,15 +1,26 @@
 from django.contrib import admin
 from django.contrib.admin.helpers import ActionForm
 from django import forms
+from django.contrib import messages
+
 
 from .models import Instrument
 
 # Register your models here.
+
+def update_price(modeladmin, request, queryset):
+    price = request.POST['price']
+    price = int(price)
+    queryset.update(price=price)
+    modeladmin.message_user(request, ("Successfully updated price for %d rows") % (queryset.count(),), messages.SUCCESS)
+update_price.short_description = 'Update price of selected rows'
+
 
 class UpdateActionForm(ActionForm):
     price = forms.IntegerField(required=False)
 
 class InstrumentAdmin(admin.ModelAdmin):
     action_form = UpdateActionForm
+    actions = [update_price]
 
 admin.site.register(Instrument, InstrumentAdmin)
