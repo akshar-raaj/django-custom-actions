21e521982900b35564e5a02f1b0e7c80117823d4
Akshar Raaj <akshar@agiliq.com>
2014-08-05 19:43:43 +0530

    Able to use a custom form as admin_form of ModelAdmin

--- instruments/admin.py
+++ instruments/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin.helpers import ActionForm
+from django import forms
 
 from .models import Instrument
 
 # Register your models here.
-admin.site.register(Instrument)
+
+class UpdateActionForm(ActionForm):
+    price = forms.IntegerField(required=False)
+
+class InstrumentAdmin(admin.ModelAdmin):
+    action_form = UpdateActionForm
+
+admin.site.register(Instrument, InstrumentAdmin)
